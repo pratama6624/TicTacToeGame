6c04ce6c6e1fb1cfa1db1f3479c77b6e3e0988ae
pratama6624 <scyoung6624@gmail.com>
2024-08-05 21:08:56 +0700

    Initial Commit

---
+++ TicTacToeGame/TicTacToeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeGameApp.swift
+//  TicTacToeGame
+//
+//  Created by Pratama One on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
